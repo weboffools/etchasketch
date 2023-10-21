f86415bebd861492d4f4b3bd2190ce602f1ef0ab
jeliotj <eliot@eliotsmith.org>
2023-10-21 17:06:20 -0600

    grid creation and cell color change features added

--- main.js
+++ main.js
@@ -8,18 +8,27 @@ function createGrid(cells) {
   for (let i = 0; i < cells; i++) {
     const rowDiv = document.createElement('div');
     rowDiv.classList.add(`row-${i}`);
-    rowDiv.setAttribute('style', `display: inline-flex; height: ${cellWidth}px`);
+    rowDiv.setAttribute('style', `display: inline-flex; height: ${cellWidth}px; margin: 0;`);
     container.appendChild(rowDiv);
 
     for (let j = 0; j < cells; j++) {
       let thisRow = document.querySelector(`.row-${i}`);
-      const cellDiv = document.createElement('div');
-      cellDiv.classList.add('celldiv');
-      cellDiv.setAttribute('style', `width: ${cellWidth}px`);
-      thisRow.appendChild(cellDiv);
+      const btn = document.createElement('button');
+      btn.classList.add('cellbtn');
+      btn.setAttribute('style', `width: ${cellWidth}px; button-style: none; border: none; margin: 0; background-color: white;`);
+      thisRow.appendChild(btn);
     }
   }
+  eventHandler();
 }
 
+function eventHandler() {
+  const buttons = document.querySelectorAll('.cellbtn')
 
+    buttons.forEach((button) => {
+      button.addEventListener('mouseover', function (e) {
+        e.target.style.backgroundColor = 'black';
+      });
+    });
+}
 
